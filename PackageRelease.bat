16fde0a2033d117a53fb32d526fffc4943a51c87
Samuel Grossman <samuelgr@users.noreply.github.com>
2026-03-29 16:05:44 -0700

    Update release packager to include Xidi plugin development SDK header files.

--- PackageRelease.bat
+++ PackageRelease.bat
@@ -4,13 +4,15 @@ setlocal enabledelayedexpansion
 set project_name=Xidi
 set project_platforms=Win32 x64
 
-set project_has_sdk=no
+set project_has_sdk=yes
 set project_has_third_party_license=yes
 
 set files_release=LICENSE README.md
 set files_release_build=dinput.dll dinput8.dll winmm.dll
 set files_release_build_Win32=Xidi.32.dll Xidi.HookModule.32.dll
 set files_release_build_x64=Xidi.64.dll Xidi.HookModule.64.dll
+
+set files_sdk_include=Include\Xidi\*.h
 
 set third_party_license=Hookshot Boost XstdBitSet
 
